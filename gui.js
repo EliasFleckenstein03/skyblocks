fbd82142dca5a7743bf2acfd4531a4b460eddf1b
Elias Fleckenstein <eliasfleckenstein@web.de>
2020-04-26 12:08:36 +0200

    GUI Enhancement

--- gui.js
+++ gui.js
@@ -9,16 +9,18 @@
 		li.style.fontSize = "20px";
 		li.innerHTML = quest.desc;
 		li.style.postion = "relative";
-		let questCount = ul.appendChild(document.createElement("var"));
+		let questCount = li.appendChild(document.createElement("small"));
 		questCount.style.position = "absolute";
-		questCount.style.right = "20px";
-		questCount.
+		questCount.style.right = "30px";
 		let checkbox = li.appendChild(document.createElement("input"));
 		checkbox.type = "checkbox";
 		checkbox.style.position = "absolute";
 		checkbox.style.right = "5px";
 		checkbox.addEventListener("input", _ => { gui.getDisplay().dispatchEvent(new Event("update")) });
-		gui.getDisplay().addEventListener("update", _ => { checkbox.checked = (dragonblocks.player.meta.skyblocksQuests[quest.name] >= quest.count)})
+		gui.getDisplay().addEventListener("update", _ => {
+			checkbox.checked = (dragonblocks.player.meta.skyblocksQuests[quest.name] >= quest.count);
+			questCount.innerHTML = Math.min(dragonblocks.player.meta.skyblocksQuests[quest.name], quest.count) + "/" + quest.count;
+		});
 	}
 	dragonblocks.menu.addButton("Skyblocks", _ => { gui.open() });
 }
